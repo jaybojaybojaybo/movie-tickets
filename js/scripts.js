33fb894adf43c3588027138ef7aa75a66832a257
Joshua Fairchild and Jasun Feddema <jasun@>
2017-03-20 19:24:19 -0700

    finish js front-end logic and define the findPrice prototype method.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,27 @@ function Ticket(movieName, movieTime, viewerAge) {
   this.viewerAge = viewerAge;
 }
 
+Ticket.prototype.findPrice = function() {
+  var ticketPrice = 15;
+  if (this.viewerAge <= 3) {
+    ticketPrice = 0;
+  } else if (this.viewerAge >= 65) {
+    ticketPrice -= 2;
+  } else if (this.viewerAge >= 4 && this.viewerAge <= 8) {
+    ticketPrice -= 3;
+  }
+
+  if (this.movieTime >= 10 && this.movieTime <= 16) {
+    ticketPrice -= 2;
+  }
+
+  if ( !($('select option:selected').hasClass('new-release')) ) {
+    ticketPrice -= 2;
+  }
+
+  return ticketPrice;
+}
+
 
 $(function() {
   $("form").submit(function(event) {
@@ -11,9 +32,12 @@ $(function() {
     var movie = $("select#movie").val();
     var time = parseInt($("input#time").val());
     var age = parseInt($("input#age").val());
+    var movieTicket = new Ticket(movie, time, age);
 
-    var movieTicket = new Ticket(movie, time, age)
-    console.log(movieTicket);
+    var ticketPrice = movieTicket.findPrice();
+    $('#output-wrapper').show();
+    $('span#output').text(ticketPrice);
+    console.log(movieTicket.movieTime);
     event.preventDefault();
   });
 });
