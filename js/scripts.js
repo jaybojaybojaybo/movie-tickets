71d6e2b8d1783abfb312158478bc01b69eec3108
Joshua Fairchild and Jasun Feddema <joshua@>
2017-03-20 18:42:52 -0700

    initialize jQuery business and front-end logic for collecting user input and creating Ticket object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function Ticket(movieName, movieTime, viewerAge) {
+  this.movieName = movieName;
+  this.movieTime = movieTime;
+  this.viewerAge = viewerAge;
+}
+
+
 $(function() {
-  
+  $("form").submit(function(event) {
+
+    var movie = $("select#movie").val();
+    var time = parseInt($("input#time").val());
+    var age = parseInt($("input#age").val());
+
+    var movieTicket = new Ticket(movie, time, age)
+    console.log(movieTicket);
+    event.preventDefault();
+  });
 });
